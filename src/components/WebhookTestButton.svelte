891ba0de89e0266d294804cb05afa4daa940c9c2
Seamus McDonald <pseamus1212@gmail.com>
2022-02-12 18:16:11 -0500

    webhook fix

--- src/components/WebhookTestButton.svelte
+++ src/components/WebhookTestButton.svelte
@@ -1,6 +1,8 @@
 <script>
 	const handleWebhookTest = async () => {
-	const res =	await fetch('/app/api/plaid/test-webhook')
+	const res =	await fetch('/app/api/plaid/test-webhook', {
+		method: 'POST',
+	})
 	const data = await res.json()
 	console.log(data)
 	}
