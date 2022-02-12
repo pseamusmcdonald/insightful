3b6a39c22d7c4e64638a009204f071a99f18ae04
Seamus McDonald <pseamus1212@gmail.com>
2022-02-12 18:19:51 -0500

    webhook fix

--- src/lib/plaid/webhook_helpers.js
+++ src/lib/plaid/webhook_helpers.js
@@ -1,14 +1,7 @@
 import db from '$lib/db'
 import { client } from '$lib/plaid'
 
-export const testWebhook = async () => {
-	const request = {
-		access_token: 'access-sandbox-69844067-992c-4cac-b3d2-11a683d6925b',
-		webhook_code: 'DEFAULT_UPDATE'
-	}
-	await client.sandboxItemFireWebhook(request)
-	return
-}
+
 
 export const handleItemError = async (item_id) => {
 	const body = await db.plaid_items.updateStatus(item_id, 'invalid')
--- src/routes/app/api/plaid/test-webhook.js
+++ src/routes/app/api/plaid/test-webhook.js
@@ -1,10 +1,16 @@
-import { testWebhook } from '$lib/plaid/webhook_helpers'
+import { client } from '$lib/plaid'
+
 
 export const post = async (req) => {
 	let error
 
-	await testWebhook()
-
+	const request = {
+		access_token: 'access-sandbox-69844067-992c-4cac-b3d2-11a683d6925b',
+		webhook_code: 'DEFAULT_UPDATE'
+	}
+	const res = await client.sandboxItemFireWebhook(request)
+	console.log(res)
+	
 	return {
 		status: !error ? 200 : 500
 	}
